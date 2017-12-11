356ae53f04acec072d2994c3a5ccce8e9ee4ced3
EngHesham <heshammedhat5@gmail.com>
2017-12-11 20:55:35 +0200

    Make proxy acquire bullet.

--- src/gun/Bullet.java
+++ src/gun/Bullet.java
@@ -1,10 +1,42 @@
 package gun;
 
+import objects.CollidableGameObject;
+
 /**
  * A single fired bullet.
  * @author H
  *
  */
-public class Bullet {
+public class Bullet extends CollidableGameObject {
+
+	public Bullet() {
+		//TODO: Make fxShape.
+	}
+
+	/**
+	 * Flag for whether this bullet is on action for collision with something.
+	 */
+	private boolean onFire;
+
+	/**
+	 * @return the onFire
+	 */
+	public final boolean isOnFire() {
+		return onFire;
+	}
+
+	/**
+	 * @param onFire the onFire to set
+	 */
+	public final void setOnFire(boolean onFire) {
+		this.onFire = onFire;
+	}
+
+	@Override
+	public void update() {
+		if (onFire) {
+			// TODO Check if the bullet hit any other collidable object.
+		}
+	}
 
 }
