3314eb1100f39b8ccabeaa841a703e3890818b85
EngHesham <heshammedhat5@gmail.com>
2017-12-12 16:31:12 +0200

    Create player class in character package

---
+++ src/characters/Player.java
@@ -0,0 +1,19 @@
+package characters;
+
+import objects.*;
+
+/**
+ * Class representing the player of the game.
+ * 
+ * @author H
+ *
+ */
+public class Player extends CollidableGameObject {
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
