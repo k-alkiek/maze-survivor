cf3c7fa9081540b54f9b3b04f8af593aa2d73d39
Khaled Alkiek <k.alkiek@gmail.com>
2017-12-13 21:53:30 +0200

    refactor states classes

--- src/characters/states/ShootingWhileWalkingState.java
+++ src/characters/states/ShootingWhileWalkingState.java
@@ -5,7 +5,7 @@ import characters.Player;
 /**
  * Created by khaledabdelfattah on 12/13/17.
  */
-public class Reloading extends State {
+public class ShootingWhileWalkingState extends State {
     @Override
     public void update(Player player) {
 
