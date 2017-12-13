36715cb2dec307cdb54ba756971ddde71dbb0edd
khaledAbdelfattah74 <khaledabdelfattah74@gmail.com>
2017-12-13 21:37:10 +0200

    added states classes

---
+++ src/characters/states/Walking.java
@@ -0,0 +1,14 @@
+package characters.states;
+
+import characters.Player;
+
+/**
+ * Created by khaledabdelfattah on 12/13/17.
+ */
+public class Walking extends State {
+
+    @Override
+    public void update(Player player) {
+
+    }
+}
