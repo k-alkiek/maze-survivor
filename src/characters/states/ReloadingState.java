e01a27922fac2633a40be99db3276a2e50c7d090
khaledAbdelfattah74 <khaledabdelfattah74@gmail.com>
2017-12-21 04:59:23 +0200

    solve shooting and reloading

--- src/characters/states/ReloadingState.java
+++ src/characters/states/ReloadingState.java
@@ -10,6 +10,8 @@ import java.io.File;
  * Created by khaledabdelfattah on 12/13/17.
  */
 public class ReloadingState extends State {
+    private int currentFrame = 0;
+
     public ReloadingState(String character) {
         super();
         String path = "src/assets/player/" + character + "/reload";
@@ -21,9 +23,11 @@ public class ReloadingState extends State {
     public void update(Player player) {
         walk(player);
         setSpritesPerFrame(player);
-
-        player.getWeapon().reload();
-
+        currentFrame++;
+        if (currentFrame >= 5) {
+            player.getWeapon().reload();
+            currentFrame = 0;
+        }
         if (framePerState == sprites.size()) {
             framePerState = 0;
             if (walking()) {
